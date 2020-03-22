5ad7b62600ab5ea070e92bc9d32806be14f30ce4
Tomasz Staniewicz <tomaszstaniewicz@gmail.com>
2020-03-22 16:12:35 +0100

    in memory auth

---
+++ src/main/java/spring/security/playground/controllers/WelcomeController.java
@@ -0,0 +1,15 @@
+package spring.security.playground.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+}
