d0015d667ea99d3e7ff4af5d462010bfae19ace1
Tomasz Staniewicz <tomaszstaniewicz@gmail.com>
2020-03-22 18:06:27 +0100

    jdbc authentication

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into users (username, password, enabled)
+            values ('user', 'user', true);
+
+insert into users (username, password, enabled)
+            values ('admin', 'admin', true);
+
+insert into authorities (username, authority)
+            values ('user', 'ROLE_USER');
+
+insert into authorities (username, authority)
+            values ('user', 'ROLE_ADMIN');
+
